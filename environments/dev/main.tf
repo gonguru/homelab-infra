5eff7ed4d121e9d291a69f34d320df9d7e0bfe47
Iván Góngora <ivan_thinkpad@os.com>
2026-04-24 05:55:08 +0000

    feat: Add CI/CD pipeline and MinIO state backend

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,6 +5,20 @@ terraform {
       version = "~> 2.25"
     }
   }
+
+  backend "s3" {
+   bucket                      = "terraform-state"
+   key                         = "dev/terraform.tfstate"
+   region                      = "us-east-1"
+   endpoint                    = "http://localhost:9000"
+   access_key                  = "minioadmin"
+   secret_key                  = "minioadmin"
+   skip_credentials_validation = true
+   skip_metadata_api_check     = true
+   skip_region_validation      = true
+   skip_requesting_account_id  = true
+   force_path_style            = true
+  }
 }
 
 provider "kubernetes" {
