d8a0702cfb276df1be8b8cc6a055f8335fddd9ed
Iván Góngora <ivan_thinkpad@os.com>
2026-04-24 06:57:41 +0000

    fix: Reformat .tf files for dev env

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,17 +8,17 @@ terraform {
   }
 
   backend "s3" {
-   bucket                      = "terraform-state"
-   key                         = "dev/terraform.tfstate"
-   region                      = "us-east-1"
-   endpoint                    = "http://localhost:9000"
-   access_key                  = "minioadmin"
-   secret_key                  = "minioadmin"
-   skip_credentials_validation = true
-   skip_metadata_api_check     = true
-   skip_region_validation      = true
-   skip_requesting_account_id  = true
-   force_path_style            = true
+    bucket                      = "terraform-state"
+    key                         = "dev/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "http://localhost:9000"
+    access_key                  = "minioadmin"
+    secret_key                  = "minioadmin"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
   }
 }
 
